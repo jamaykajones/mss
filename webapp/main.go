560038a105f97f43836543ba56bf08c7c4846745
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-09-01 09:44:06 -0400

    data driven view layer - loading templates

--- webapp/main.go
+++ webapp/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //handles every URL in app
+		requestedFile := r.URL.Path[1:]                //slicing off 1st char ("/")
+		t := templates.Lookup(requestedFile + ".html") // adds .html so user doesn't have to
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound) // template not found
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template { // returns a single template back out
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
